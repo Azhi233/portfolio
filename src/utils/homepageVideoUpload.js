d24131077ae9259f7ba33bcf05b219fabc80dedf
zhizhi <moses233@qq.com>
2026-04-29 00:15:00 +0800

    fix homepage video upload root

--- src/utils/homepageVideoUpload.js
+++ src/utils/homepageVideoUpload.js
@@ -1,5 +1,5 @@
 import { uploadMediaAsset } from './projectVideoUpload.js';
 
 export async function uploadHomepageVideo(file, handlers = {}) {
-  return uploadMediaAsset(file, { type: 'public', ...handlers });
+  return uploadMediaAsset(file, { type: 'public', root: 'Homepage Video', assetSpace: 'Homepage Video', category: 'Homepage Video', ...handlers });
 }
